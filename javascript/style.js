a00aec10501639da86debcb28ca164b48cba5946
Juan Rosales <rosales97j@gmail.com>
2021-07-27 01:13:20 -0400

    well that was tough

--- javascript/style.js
+++ javascript/style.js
@@ -12,64 +12,93 @@ switchButton.addEventListener('click', () =>{
 });
 
 
-//----------------calculator buttons-------------------
+//----------------calculator class-------------------
+//idea taken from https://github.com/WebDevSimplified/Vanilla-JavaScript-Calculator
+
+class calculator{
+  constructor(currentNum){
+    this.currentNum = currentNum
+    this.previousNum = ''
+    this.resetNumbers()
+  }
+  resetNumbers(){
+    this.currentDisplay = ''
+    this.previousDisplay = ''
+  }
+  appendNumbers (number){
+    if(number === '.' && this.currentNum.innerHTML.includes('.')) return
+    this.currentDisplay = this.currentNum.innerHTML.toString() + number.toString()
+  }
+  compute() {
+    let total = 0
+    let prev = +this.previousDisplay
+    let curr = +this.currentDisplay
+    console.log(prev)
+    console.log(curr)
+    switch(this.operation){
+      case '+':
+        total = prev + curr
+        break;
+      case '-':
+        total = prev - curr
+        break;
+      case 'x':
+        total = prev * curr
+        break;
+      case '/':
+        total = prev / curr
+        break;
+      default:
+        return
+    }
+    this.currentDisplay = total
+  }
+
+  setOpeartion (operation){
+    this.operation = operation
+    this.previousDisplay = this.currentDisplay
+    this.currentDisplay = ''
+  }
+  
+  updateDisplay (){
+    this.currentNum.innerHTML = this.currentDisplay
+  }
+}
+
+
 const numberButton = document.querySelectorAll('[data-number]')
 const operationButton = document.querySelectorAll('[data-operation]')
 const equalButton = document.querySelector('[data-equal]')
 const deleteButton = document.querySelector('[delete-button]')
 const resetButton = document.querySelector('[data-reset]')
-const displayResult = document.querySelector('.result')
+const currentNum = document.querySelector('.result')
+const previousNum = document.querySelector('.prev-result')
 
-const displayNumbers = (number) =>{
-  if(number === '.' && displayResult.innerHTML.includes('.')) return
-  displayResult.innerHTML = displayResult.innerHTML.toString() + number.toString()
-
-};
-
-const resetNumbers = () =>{
-  if (displayResult.innerHTML == '') return;
-  displayResult.innerHTML = ''
-}
-
-const solveOpeartion = (operation, number) => {
-  
-  switch (operation){
-    case '+':
-      console.log(+number)
-      break;
-    case '-':
-      console.log(+number)
-      break;
-    case 'x':
-      console.log(+number)
-      break;
-    case '/':
-      console.log(+number)
-      break;
-  }
-}
-
-/** 
-const updateDisplay = () =>{
-  displayResult.innerHTML = displayResult.innerHTML
-};
-*/
+const myCalculator = new calculator(currentNum)
 
 numberButton.forEach(button =>{
   button.addEventListener('click', () =>{
-    displayNumbers(button.innerHTML);
+    myCalculator.appendNumbers(button.innerHTML)
+    myCalculator.updateDisplay()
   })
 })
 
-resetButton.addEventListener('click', resetNumbers);
 
 operationButton.forEach(button =>{
   button.addEventListener('click', () =>{
-    //let saved = displayResult.innerHTML
-    displayNumbers(button.innerHTML)
-    //solveOpeartion(button.innerHTML, saved)
+    myCalculator.setOpeartion(button.innerHTML)
+    myCalculator.updateDisplay();
   })
 })
 
+resetButton.addEventListener('click', button =>{
+  myCalculator.resetNumbers()
+  myCalculator.updateDisplay()
+})
+
+equalButton.addEventListener('click', button =>{
+  myCalculator.compute()
+  myCalculator.updateDisplay()
+})
 //equalButton.addEventListener('click', )
   
